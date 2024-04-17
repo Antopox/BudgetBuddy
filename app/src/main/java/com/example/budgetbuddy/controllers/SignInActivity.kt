eeb10229cef32e534882c1bb5601169c17feaf1a
anton <antonioaguilaresteban1@gmail.com>
2024-04-17 12:12:18 +0200

    implemented drawer menu to the activitys

--- app/src/main/java/com/example/budgetbuddy/controllers/SignInActivity.kt
+++ app/src/main/java/com/example/budgetbuddy/controllers/SignInActivity.kt
@@ -25,11 +25,6 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         auth = FirebaseAuth.getInstance()
 
         txtUserName = findViewById(R.id.etRegisterName)
